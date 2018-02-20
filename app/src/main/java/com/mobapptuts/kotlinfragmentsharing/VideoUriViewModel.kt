1887c0f9f84e15286002204b4bb98e27bbec726d
Nigel Henshaw <nigel@mobapptuts.com>
2018-02-20 11:08:59 +0900

    Implemented ViewModal class

---
+++ app/src/main/java/com/mobapptuts/kotlinfragmentsharing/VideoUriViewModel.kt
@@ -0,0 +1,11 @@
+package com.mobapptuts.kotlinfragmentsharing
+
+import android.arch.lifecycle.ViewModel
+import android.net.Uri
+
+/**
+ * Created by nigelhenshaw on 2018/02/20.
+ */
+class VideoUriViewModel: ViewModel() {
+    var videoUri: Uri? = null
+}
